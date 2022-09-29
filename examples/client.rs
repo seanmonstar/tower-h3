b72cae4e2781ad50146a485d34886e78a7a174b2
Julian Eager <eagr@tutanota.com>
2022-09-29 18:01:38 +0800

    integrate Endpoint into example

--- examples/client.rs
+++ examples/client.rs
@@ -1,22 +1,30 @@
+use std::error::Error;
+use std::net::SocketAddr;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
-use futures::future;
-use h3_quinn::quinn;
+use futures::future::{self, Future};
 use http_body::Body;
 use tokio::io::AsyncWriteExt;
+use tower_h3::client::Endpoint;
 use tower_service::Service;
 
 static ALPN: &[u8] = b"h3";
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
         .with_writer(std::io::stderr)
         .init();
 
-    let dest = std::env::args().nth(1).expect("uri required via CLI").parse::<http::Uri>().unwrap();
+    let dest = std::env::args()
+        .nth(1)
+        .expect("uri required via CLI")
+        .parse::<http::Uri>()
+        .unwrap();
 
     if dest.scheme() != Some(&http::uri::Scheme::HTTPS) {
         Err("destination scheme must be 'https'")?;
@@ -42,6 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_safe_default_cipher_suites()
         .with_safe_default_kx_groups()
         .with_protocol_versions(&[&rustls::version::TLS13])?;
+
     let mut roots = rustls::RootCertStore::empty();
     match rustls_native_certs::load_native_certs() {
         Ok(certs) => {
@@ -55,21 +64,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             eprintln!("couldn't load any default trust roots: {}", e);
         }
     };
+
     let mut tls_config = tls_config_builder
         .with_root_certificates(roots)
         .with_no_client_auth();
     tls_config.enable_early_data = true;
     tls_config.alpn_protocols = vec![ALPN.into()];
-    let client_config = quinn::ClientConfig::new(Arc::new(tls_config));
 
-    let mut client_endpoint = h3_quinn::quinn::Endpoint::client("[::]:0".parse().unwrap())?;
-    client_endpoint.set_default_client_config(client_config);
-    let quinn_conn = h3_quinn::Connection::new(client_endpoint.connect(addr, auth.host())?.await?);
+    let client_config = quinn::ClientConfig::new(Arc::new(tls_config));
+    let mut quinn_endpoint = quinn::Endpoint::client("[::]:0".parse().unwrap())?;
+    quinn_endpoint.set_default_client_config(client_config);
+
+    let conn = Connect::new(quinn_endpoint.clone());
+
+    // TODO make type inference work and remove this
+    let mut endpoint: Endpoint<Connect, http_body::Empty<bytes::Bytes>> = Endpoint::new(conn);
+    let (mut driver, mut service) = endpoint.call((addr, auth.host())).await?;
 
     eprintln!("QUIC connected ...");
-
-    // generic h3
-    let (mut driver, mut send_request) = h3::client::new(quinn_conn).await?;
 
     tokio::spawn(async move {
         if let Err(e) = future::poll_fn(|cx| driver.poll_close(cx)).await {
@@ -77,14 +89,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    let mut svc = tower_h3::client::Connection::new(send_request);
-
     eprintln!("Sending request ...");
     let req = http::Request::builder()
         .uri(dest)
         .body(http_body::Empty::new())?;
 
-    let mut resp = svc.call(req).await?;
+    let mut resp = service.call(req).await?;
 
     eprintln!("Received response ...");
 
@@ -98,8 +108,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         out.flush().await.expect("flush");
     }
 
-    drop(svc);
-    client_endpoint.wait_idle().await;
+    drop(service);
+    quinn_endpoint.wait_idle().await;
 
     Ok(())
+}
+
+pub struct Connect {
+    inner: h3_quinn::quinn::Endpoint,
+}
+
+impl Connect {
+    pub fn new(inner: h3_quinn::quinn::Endpoint) -> Self {
+        Self { inner }
+    }
+}
+
+impl Service<(SocketAddr, &str)> for Connect {
+    type Response = h3_quinn::Connection;
+    type Error = Box<dyn Error>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, dst: (SocketAddr, &str)) -> Self::Future {
+        let (addr, server_name) = dst;
+        let connecting = self.inner.connect(addr, server_name);
+
+        Box::pin(async move {
+            let conn = connecting?.await?;
+            Ok(h3_quinn::Connection::new(conn))
+        })
+    }
 }
--- src/client/endpoint.rs
+++ src/client/endpoint.rs
@@ -1,7 +1,10 @@
 use std::marker::PhantomData;
 use std::pin::Pin;
-use std::task::{self, Poll};
+use std::task::{Context, Poll};
 
+use futures::future::Future;
+use h3::quic;
+use http_body::Body;
 use tower_service::Service;
 
 pub struct Endpoint<C, B> {
@@ -9,35 +12,46 @@ pub struct Endpoint<C, B> {
     _body: PhantomData<fn() -> fn(B)>,
 }
 
-impl<C, Dst, O, B> Service<Dst> for Endpoint<C, B>
+impl<C, B> Endpoint<C, B> {
+    pub fn new(inner: C) -> Self {
+        Self {
+            inner,
+            _body: PhantomData,
+        }
+    }
+}
+
+impl<C, D, O, B> Service<D> for Endpoint<C, B>
 where
-    C: Service<Dst>,
-    C::Response: h3::quic::Connection<B::Data, OpenStreams = O> + Send + 'static,
+    C: Service<D>,
+    C::Response: quic::Connection<B::Data, OpenStreams = O> + Send + 'static,
     C::Future: Send + 'static,
-    O: h3::quic::OpenStreams<B::Data> + Send + 'static,
-    <C::Response as h3::quic::Connection<B::Data>>::SendStream: Send + 'static,
-    B: http_body::Body + Send + 'static,
+    O: quic::OpenStreams<B::Data> + Send + 'static,
+    <C::Response as quic::Connection<B::Data>>::SendStream: Send + 'static,
+    <C::Response as quic::Connection<B::Data>>::RecvStream: Send + 'static,
+    B: Body + Send + 'static,
     B::Data: Send + 'static,
 {
-    type Response = super::Connection<O, B>;
+    type Response = (
+        h3::client::Connection<C::Response, B::Data>,
+        super::Connection<O, B>,
+    );
     type Error = h3::Error;
-    type Future = Pin<Box<dyn std::future::Future<Output=Result<Self::Response, Self::Error>> + Send>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-
-    fn poll_ready(&mut self, _cx: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, dst: Dst) -> Self::Future {
+    fn call(&mut self, dst: D) -> Self::Future {
         let connecting = self.inner.call(dst);
 
         Box::pin(async move {
-            let conn = connecting.await.unwrap_or_else(|_| panic!("connecting error"));
-
-            let (driver, tx) = h3::client::builder()
-                .build(conn)
-                .await?;
-            Ok(super::Connection::new(tx))
+            let conn = connecting
+                .await
+                .unwrap_or_else(|_| panic!("connecting error"));
+            let (driver, send_request) = h3::client::builder().build(conn).await?;
+            Ok((driver, super::Connection::new(send_request)))
         })
     }
 }
